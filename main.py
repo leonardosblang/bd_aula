7dadc424e6ee9c3dd49da09b885500e1f98a164a
Leolang <leolang99@gmail.com>
2023-03-05 03:51:35 -0300

    aula 4

--- main.py
+++ main.py
@@ -1,98 +1,33 @@
-# This is a sample Python script.
+from save_json import writeAJson
+from database import Database
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# idade = 20
-# dinheiro = 1.5
-# nome = "leonardo"
-# idade2 = "10"
-# print()
+db = Database(database="dex", collection="pokemons")
+db.reset_database()
+pokemons = db.collection.find()
 
 
-# lista_vazia = []
-# lista1 = [1,6,9,1]
-# print(lista1[0])
-# lista1.append(8)
-# print(lista1)
-# lista1[0] = 7
-# print(lista1)
-# del lista1[0]
-# print(lista1)
+# for pokemon in pokemons:
+# print(pokemon)
 
-# cores = {'vermelho': '#FF0000', 'azul': '#0000FF', 'verde': '#00FF00'}
-# print(cores)
-# print(cores['vermelho'])
-# cores['amarelo'] = '#FFFF00'
-# print(cores['amarelo'])
-# print(cores)
-# cores['vermelho'] = '#6b0000'
-# print(cores)
-#
-# print(cores['amarelo'])
-# del cores['amarelo']
-# print(cores)
-
-# def multiplicar_por_dois(x,y):
-#     return (str(x*2),str(y*2))
-# funcao = multiplicar_por_dois(5,9)
-# print(funcao)
-#
-# numeros = [1, 2, 3, 4, 5]
-# for numero in numeros:
-#     print(numero)
-#
-# cores = {'vermelho': '#FF0000', 'azul': '#0000FF', 'verde': '#00FF00'}
-#
-# for key, value in cores.items():
-#     print(key + ': ' + value)
-#
-# counter = 0
-# while counter<= 10:
-#     print(counter)
-#     counter = counter + 1
-#
-# numero = -6
-# if numero > 0:
-#     print('O número é positivo')
-# else:
-#     print('O número é zero')
-
-class Cachorro:
-     tipo = "Yorkshire"
-
-     def __init__(self,nome,idade,latido):
-         self.nome = nome
-         self.idade = idade
-         self.latido = latido
-     def latir(self):
-        print(self.nome,"está latindo :",self.latido)
-
-     def print_nome(self):
-        print("nome" +"= " +self.nome)
-
-cachorro1 = Cachorro("Bob", 17,"Au Au")
-print(cachorro1.nome)
-print(cachorro1.idade)
-cachorro1.nome = "Micheal"
-print(cachorro1.nome)
-print(cachorro1.tipo)
-print(cachorro1.latir())
-print(cachorro1.print_nome())
-
-class Cachorro_Grande(Cachorro):
-    def latir(self):
-        self.latido = (self.latido+" ALTO")
-        print((self.nome+ " está latindo :" + self.latido))
-
-    def print_nome(self):
-        print("nome do cachorro grande " + "= " + self.nome)
+def getPokemonByDex(number: int):
+    return db.collection.find({"id": number})
 
 
-cachorro2 = Cachorro_Grande("Big Bob",22,"AU AU")
-print(cachorro2.nome)
-print(cachorro2.latido)
-print(cachorro2.idade)
-print(cachorro2.latir())
-print(cachorro2.print_nome())
+bulbasaur = getPokemonByDex(1)
+writeAJson(bulbasaur, "bulbasaur")
+
+
+pokemon = db.collection.find({"type": "Grass", "base.Attack": { "$lte": 50 }})
+writeAJson(pokemon, "pokemon_grass")
+
+
+def get_4_letters_or_less(collection):
+  names = collection.find({}, {"name": 1})
+  four_letters_or_less = []
+  for name in names:
+    if len(name["name"].keys()) <= 4:
+      if all(len(word) <= 4 for word in name["name"].values()):
+        four_letters_or_less.append(name["name"].values())
+  return four_letters_or_less
+
+writeAJson(get_4_letters_or_less(db.collection), "pokemon_4_words_or_less")
